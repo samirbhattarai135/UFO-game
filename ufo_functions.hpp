4f393f41755c31f90a2ac7b3fdca7ccd724dc134
samirbhattarai135 <147074565+samirbhattarai135@users.noreply.github.com>
2023-12-20 23:31:09 -0600

    Add files via upload

---
+++ ufo_functions.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+void display_misses(int misses);
+void greet();
+// Declare functions
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
